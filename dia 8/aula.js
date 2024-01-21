52d02284031a6aa9af63fe3df03ee2654dd33ed5
Hiago Candido da Silva <contatohchiago@gmail.com>
2024-01-21 19:31:15 -0300

    dia 8 do curso

---
+++ dia 8/aula.js
@@ -0,0 +1,10 @@
+// function Somar(num1, num2){
+//     return num1 + num2
+// }
+// let resultSoma = Somar(10,31)
+
+function ExibirNome(nome){
+    return nome
+}
+let nome = 'hiago'
+ExibirNome(nome)
